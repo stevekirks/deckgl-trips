eaa7a3e68d55381a3505b3b2428c0e67c511c77a
stevekirks <SJKirkegard@gmail.com>
2019-09-08 03:40:53 +0000

    v1.0.44

---
+++ precache-manifest.d0fb80349b9c4dae3e0b35e702651737.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8107757cfa4e7026f14699408dc30a9",
+    "url": "./index.html"
+  },
+  {
+    "revision": "b12b7bb53bdca6b1196b",
+    "url": "./static/css/2.6639d161.chunk.css"
+  },
+  {
+    "revision": "91303a9d63c4adf781f7",
+    "url": "./static/css/main.6c2548ca.chunk.css"
+  },
+  {
+    "revision": "b12b7bb53bdca6b1196b",
+    "url": "./static/js/2.24ae7e3b.chunk.js"
+  },
+  {
+    "revision": "91303a9d63c4adf781f7",
+    "url": "./static/js/main.666592d3.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
