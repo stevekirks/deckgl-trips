7e408d21b7b92a8eb0f0be4eeca71a30ea755c4f
stevekirks <5696096+stevekirks@users.noreply.github.com>
2021-07-03 15:32:23 +1000

    Library and framework upgrades

--- src/typings/react-map-gl.d.ts
+++
@@ -1,7 +0,0 @@
-declare module 'react-map-gl' {
-    var MapGL: any;
-    export default MapGL;
-    export class Popup extends React.Component<any, any> { 
-        constructor(args: any)
-    }
-}
