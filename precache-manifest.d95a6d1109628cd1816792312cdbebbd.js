3d5bd49be48cec4337e8687a7d11384ab4c2fe53
stevekirks <SJKirkegard@gmail.com>
2021-07-03 07:32:46 +0000

    v1.0.82

---
+++ precache-manifest.d95a6d1109628cd1816792312cdbebbd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95795cc46618de899d5d48c3179266d4",
+    "url": "./index.html"
+  },
+  {
+    "revision": "7e32759947bdb490027a",
+    "url": "./static/css/2.dd221cdc.chunk.css"
+  },
+  {
+    "revision": "90d082342e36f94b52f5",
+    "url": "./static/css/main.533df5b1.chunk.css"
+  },
+  {
+    "revision": "7e32759947bdb490027a",
+    "url": "./static/js/2.c27844d3.chunk.js"
+  },
+  {
+    "revision": "7bfcea31855bd57860628e5726359b29",
+    "url": "./static/js/2.c27844d3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "90d082342e36f94b52f5",
+    "url": "./static/js/main.f5822962.chunk.js"
+  },
+  {
+    "revision": "091c6d45e6d4d5a663ed",
+    "url": "./static/js/runtime-main.3f9d59f8.js"
+  }
+]);
