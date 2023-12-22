643d98ef4494207151498116f43f7eb24f1ab1f1
stevekirks <5696096+stevekirks@users.noreply.github.com>
2023-12-23 08:47:40 +1000

    Package updates

--- src/index.tsx
+++ src/index.tsx
@@ -2,22 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './app';
-import reportWebVitals from './report-web-vitals';
 
-// Workaround: these 4 lines are to fix issue https://github.com/mapbox/mapbox-gl-js/issues/10565
-// Install packages worker-loader & mapbox-gl
-import mapboxgl from "mapbox-gl";
-// eslint-disable-next-line import/no-webpack-loader-syntax
-(mapboxgl as any).workerClass = require('worker-loader!mapbox-gl/dist/mapbox-gl-csp-worker').default;
+// // Workaround: these 4 lines are to fix issue https://github.com/mapbox/mapbox-gl-js/issues/10565
+// // Install packages worker-loader & mapbox-gl
+// import mapboxgl from "mapbox-gl";
+// // eslint-disable-next-line import/no-webpack-loader-syntax
+// (mapboxgl as any).workerClass = require('worker-loader!mapbox-gl/dist/mapbox-gl-csp-worker').default;
 
-const root = ReactDOM.createRoot(document.getElementById('root')!);
-root.render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  </React.StrictMode>,
+)
